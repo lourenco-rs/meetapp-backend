87149e9e68141bf90dfa99f0bde292dacfdd619c
Rodrigo Sinope 70239 <lourenco.rs@gmail.com>
2019-09-04 19:00:07 -0300

    Cadastro de usuarios

---
+++ src/app/controllers/UserController.js
@@ -0,0 +1,33 @@
+import * as yup from 'yup';
+import User from '../models/User';
+
+class UserController {
+  async create(req, res) {
+    const schema = yup.object().shape({
+      name: yup.string().required(),
+      email: yup.string().required(),
+      password: yup
+        .string()
+        .required()
+        .min(6),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation fails' });
+    }
+
+    const userExists = await User.findOne({
+      where: { email: req.body.email },
+    });
+
+    if (userExists) {
+      return res.status(400).json({ error: 'User already registered' });
+    }
+
+    const { id, name, email } = await User.create(req.body);
+
+    return res.json({ id, name, email });
+  }
+}
+
+export default new UserController();
--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,13 @@
 import { Router } from 'express';
 
+import UserController from './app/controllers/UserController';
+
 const routes = new Router();
 
 routes.get('/', (req, res) => {
   return res.json({ message: 'Welcome to Meetapp' });
 });
+
+routes.post('/users', UserController.create);
 
 export default routes;
