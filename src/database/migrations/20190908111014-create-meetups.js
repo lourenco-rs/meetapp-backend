77ee44491b02f76c06f30fffb8c5ad89356580c1
Rodrigo Sinope 70239 <lourenco.rs@gmail.com>
2019-09-09 14:07:31 -0300

    Migration e model de meetup

---
+++ src/database/migrations/20190908111014-create-meetups.js
@@ -0,0 +1,60 @@
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('meetups', {
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      banner_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'files',
+          key: 'id',
+        },
+        onUpdate: 'cascade',
+        onDelete: 'restrict',
+      },
+      user_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'users',
+          key: 'id',
+        },
+        onUpdate: 'cascade',
+        onDelete: 'restrict',
+      },
+      title: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      description: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      location: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      date: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+    });
+  },
+
+  down: queryInterface => {
+    return queryInterface.dropTable('meetups');
+  },
+};
