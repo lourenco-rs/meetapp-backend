84c2296fd3c04f7219c5725f73c787c6b495b887
Rodrigo Lourenço <lourenco.rs@gmail.com>
2019-10-27 17:06:09 -0300

    Configurando cors

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import 'dotenv/config';
 
 import express from 'express';
 import path from 'path';
+import cors from 'cors';
 
 import routes from './routes';
 import './database/loader';
@@ -14,6 +15,7 @@ class App {
   }
 
   middlewares() {
+    this.server.use(cors());
     this.server.use(express.json());
     this.server.use(
       '/files',
